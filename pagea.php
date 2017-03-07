30b59b26e8d7a9953a4a2cdbc95f2b4a2f53932f
comscisau-wichuphon <s5752410047@sau.ac.th>
2017-03-07 14:09:26 +0700

    update #1 na ja

---
+++ pagea.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>pagea</title>
+</head>
+<body>
+<h1>pagea</h1>
+	
+</body>
+</html>
---
+++ pageb.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>pageb</title>
+</head>
+<body>
+<h1>pageb</h1>
+	
+</body>
+</html>
